4eead318cfa26e1232671d05c0c7da3470d254c4
Hinbin <nicholashoulton@gmail.com>
2018-02-28 16:17:05 +0000

    First setup of Jest firebase-server unit testing

--- src/Actions/LiveLeaderboardActions.test.js
+++ src/Actions/LiveLeaderboardActions.test.js
@@ -1,19 +1,21 @@
 import firebase from 'firebase'
 import FirebaseServer from 'firebase-server'
 import detect from 'detect-port'
-import Jest from 'jest'
 
 export async function startFirebaseTestServer () {
     let server
-  const port = await detect(5000)
-  if (port === 5000) {
+    const port = await detect(5000)
+    if (port === 5000) {
         server = new FirebaseServer(5000, '127.0.0.1', {})
-  }
+    }
     firebase.initializeApp({ databaseURL: 'ws://127.0.1:5000' })
-  const dbRef = firebase.app().database().ref()
-  return { server, dbRef }
+    const dbRef = firebase.app().database().ref()
+    return { server, dbRef }
 }
+
 let dbRef
+
 beforeAll(async () => {
     ({ dbRef } = await startFirebaseTestServer())
+    console.log(dbRef)
 })
